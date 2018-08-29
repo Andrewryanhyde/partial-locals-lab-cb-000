14aa26e6f80dbf4e1fb6cacd119dc83d73ad6e5a
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-08-29 09:00:59 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,16 +1,29 @@
-# == Schema Information
-#
-# Table name: students
-#
-#  id         :integer          not null, primary key
-#  name       :string
-#  hometown   :string
-#  birthday   :datetime
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(input)
+    if input.empty?
+      Student.all
+    else
+      matches = []
+      Student.all.each do |student|
+
+        s = student.name.scan(/#{input}/i).first
+      
+        if s != nil
+          matches << student
+        end
+      end
+
+      matches
+    end
+
+
+
+
+
+  end
+
+
 end
